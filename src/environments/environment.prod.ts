3e22008cd5402689925d044d76db2b24290a8154
Jefferson Pozo <misaelpozocatuto@gmail.com>
2026-09-21 21:59:26 -0500

    fix: first commit claude front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://apicasablanca.onrender.com/api/v1'
+  apiUrl: 'https://casa-blanca-back-ssv42x-3cefed-34-66-70-48.sslip.io/api/v1',
 };
